7d5a855b74aa8e4a90e71bf9202b269e0ad7abcd
Victor Hugo <vhaporfiro@gmail.com>
2025-02-12 15:15:29 -0300

    Obtaining a user by id.

---
+++ src/main/java/com/vhaporfiro/workshop/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.vhaporfiro.workshop.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+
+		super(msg);
+        
+	}
+}
