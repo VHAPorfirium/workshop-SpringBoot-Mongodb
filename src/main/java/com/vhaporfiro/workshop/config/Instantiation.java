db0cb739f204b37c19bd653d0bd5a5360527469c
Victor Hugo <vhaporfiro@gmail.com>
2025-02-15 10:39:26 -0300

    Classe Post

--- src/main/java/com/vhaporfiro/workshop/config/Instantiation.java
+++ src/main/java/com/vhaporfiro/workshop/config/Instantiation.java
@@ -1,30 +1,45 @@
 package com.vhaporfiro.workshop.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.vhaporfiro.workshop.domain.Post;
 import com.vhaporfiro.workshop.domain.User;
+import com.vhaporfiro.workshop.repository.PostRepository;
 import com.vhaporfiro.workshop.repository.UserRepository;
 
 @Configuration
-public class Instantiation implements CommandLineRunner{
+public class Instantiation implements CommandLineRunner {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userReposiroty;
 
-    @Override
-    public void run(String... args) throws Exception {
+	@Autowired
+	private PostRepository postReposiroty;
 
-        userRepository.deleteAll();
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		userReposiroty.deleteAll();
+		postReposiroty.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
 
-        User maria = new User(null, "Maria Brown", "maria@gmail.com");
-        User alex = new User(null, "Alex Green", "alex@gmail.com");
-        User bob = new User(null, "Bob Grey", "bob@gmail.com");
-        
-        userRepository.saveAll(Arrays.asList(maria, alex, bob));
-    }
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+		postReposiroty.saveAll(Arrays.asList(post1, post2));
+	}
 
 }
---
+++ src/main/java/com/vhaporfiro/workshop/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.vhaporfiro.workshop.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vhaporfiro.workshop.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
